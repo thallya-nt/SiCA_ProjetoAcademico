18156ade94484726390c213cd2ba7e8bea38ad8c
thallya-nt <thallyanatielly.fs@gmail.com>
2026-09-21 17:32:20 -0300

    doc: adicona comentários explicativos na classe Client

--- src/sockets/Client.java
+++ src/sockets/Client.java
@@ -2,21 +2,24 @@ package sockets;
 
 import java.io.*;
 import java.net.*;
-import java.util.IllegalFormatCodePointException;
 import java.util.Scanner;
 
 public class Client {
-    private static final String SERVER_IP = "127.0.0.1";
+    //CONFIGURAÇÕES DO SERVIDOR
+    private static final String SERVER_IP = "127.0.0.1";       //LOCALHOST
     private static final int DOOR = 2026;
     private static final String DIRETORIO_CLIENT = "./arquivos_cliente/";
 
     public static void main(String[] args) {
+        //CRIA PASTA ONDE OS ARQUIVOS FICARÃO, CASO NÃO EXISTA
         File diretorio = new File(DIRETORIO_CLIENT);
         if (!diretorio.exists()) {
             diretorio.mkdirs();
         }
+        //RECEBE A/AS OPÇÃO/OPÇÕES ESCOLHIDA PELO CLIENTE
         Scanner scanner = new Scanner(System.in);
 
+        // MENU EM LOOP NO TERMINAL
         while (true) {
             System.out.println("\n ### ESCOLHA UMA OPÇÃO ###");
             System.out.println("1. LISTAR ARQUIVOS;");
@@ -33,10 +36,12 @@ public class Client {
                 break;
             }
 
+            //ABRE CONEXÃO SOCKET COM O SERVIDOR A CADA OPERAÇÃO
             try (Socket socket = new Socket(SERVER_IP, DOOR);
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream())){
+            DataInputStream in = new DataInputStream(socket.getInputStream()); //RECEBER DADOS
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream())){ // ENVIAR DADOS
 
+                //ACIONA O METODO CORRESPONDENTE A OPÇÃO ESCOLHIDA PELO CLIENTE
                 switch (opcao) {
                     case 1:
                         listarArquivos(out, in);
@@ -58,10 +63,11 @@ public class Client {
                 System.out.println("ERRO NA COMUNICAÇÃO" + e.getMessage());
             }
         }
-        scanner.close();
+        scanner.close(); //FECHA O SCANNER
     }
+    //METODO RESPONSÁVEL POR LISTAR OS ARQUIVOS PRESENTES NO SERVIDOR
     private static void listarArquivos(DataOutputStream out, DataInputStream in) throws IOException{
-        out.writeUTF("LIST");
+        out.writeUTF("LIST"); // INDICA QUAL COMANDO ESTÁ SENDO PEDIDO PELO CLIENTE
 
         int quantidade = in.readInt();
         System.out.println("\n- - ARQUIVOS NO SERVIDOR ["+ quantidade + "] - -");
@@ -69,17 +75,19 @@ public class Client {
             System.out.println("- "+ in.readUTF());
         }
     }
+    //METODO RESPONSÁVEL POR ENVIAR ARQUIVOS PARA O SERVIDOR
     private static void enviarArquivos(DataOutputStream out, String nDoc) throws IOException {
         File doc = new File(DIRETORIO_CLIENT + nDoc);
 
-        if (!doc.exists() || !doc.isFile()){
+        if (!doc.exists() || !doc.isFile()){ //VERIFICA SE O ARQUIVO EXISTE NA PASTA DO CLIENTE
             System.out.println("ARQUIVO NÃO ENCONTRADO.");
             return;
         }
-        out.writeUTF("UPLOAD".trim());
+        out.writeUTF("UPLOAD".trim()); // INDICA O COMANDO PEDIDO PELO CLIENTE
         out.writeUTF(doc.getName());
         out.writeLong(doc.length());
 
+        //LÊ O ARQUIVO EM BLOCOS DE BYTE
         try (FileInputStream fin = new FileInputStream(doc)){
             byte [] buffer = new byte[4096];
             int bytesLidos;
@@ -89,23 +97,27 @@ public class Client {
         }
         System.out.println("UPLOAD CONCLUÍDO");
     }
-    private static void baixarArquivos(DataOutputStream out, DataInputStream in, String nDoc) throws IOException {
-        out.writeUTF("DOWNLOAD");
-        out.writeUTF(nDoc);
+    //METODO RESPONSÁVEL POR RECEBER ARQUIVOS DO SERVIDOR
+    private static void baixarArquivos(DataOutputStream out, DataInputStream in, String nDoc)
+            throws IOException {
+        out.writeUTF("DOWNLOAD"); // INDICA QUAL COMANDO O CLIENTE SOLICITOU
+        out.writeUTF(nDoc); // NOME DO ARQUIVO QUE SERÁ BAIXADO
 
+        //VERIFICA SE O ARQUIVO EXISTE
         boolean exist = in.readBoolean();
         if (!exist) {
             System.out.println("O ARQUIVO SOLICITADO NÃO EXISTE.");
             return;
         }
         long dSize = in.readLong();
-        File doc = new File(DIRETORIO_CLIENT + nDoc);
+        File doc = new File(DIRETORIO_CLIENT + nDoc); //SALVA O ARQUIVO NO DISCO LOCAL
 
         try (FileOutputStream fout = new FileOutputStream(doc)){
             byte[] buffer = new byte[4096];
             int bytesLidos;
-            long allRead = 0;
+            long allRead = 0;  //CONTROLADOR DE BYTES RECEBIDOS
 
+            //CONTINUA RECEBENDO BYTES ATÉ ATINGIR A QUANTIDADE ESPERADA
             while (allRead < dSize && (bytesLidos = in.read(buffer, 0, (int)
                     Math.min(buffer.length, dSize - allRead))) != -1){
                 fout.write(buffer, 0, bytesLidos);
